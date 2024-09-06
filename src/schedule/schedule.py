8820d92660e9b9eb81e1baad33016ee828a2aa0b
DSHakirov <shakirovdominatus@gmail.com>
2024-09-06 09:58:38 +0300

    fixes

--- src/schedule/schedule.py
+++ src/schedule/schedule.py
@@ -1,6 +1,6 @@
-from aiogram.exceptions import TelegramBadRequest, TelegramServerError
+from aiogram.exceptions import TelegramBadRequest, TelegramServerError, TelegramForbiddenError
 from bot_logger import init_logger
-from services.user_services import get_all_users_tg_ids
+from services.user_services import get_all_users_tg_ids, set_user_is_blocked
 from dispatcher import bot
 from services.schedule_services import get_survey_reminder_text
 from keyboards.keyboards import survey_reminder_kb
@@ -18,6 +18,9 @@ async def send_survey_notification():
             await bot.send_message(tg_id, message_text, reply_markup=survey_reminder_kb())
         except (TelegramBadRequest, TelegramServerError) as e:
             logger.debug(f"failed to send message to {tg_id}: {e}")
+        except TelegramForbiddenError:
+            logger.info(f"User {tg_id} has blocked bot")
+            await set_user_is_blocked(tg_id)
 
 
 
